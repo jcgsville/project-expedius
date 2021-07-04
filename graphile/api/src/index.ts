7a557fc520debf55111fa75fc8268a7233d5e9a5
Chandler Gonzales <cgonzales@logixboard.com>
2021-07-03 19:06:09 -0700

    chore: switch from fastify to express

--- graphile/api/src/index.ts
+++ graphile/api/src/index.ts
@@ -1,35 +1,27 @@
-import fastify from 'fastify'
+import express from 'express'
 import { postgraphile } from 'postgraphile'
-import { addPostgraphileToFastify } from './add-postgraphile-to-fastify'
 import { generatePostgraphileOptions } from './postgraphile-options'
 import { constructPgPool } from './utils/construct-pg-pool'
 import { envVarToBool, portFromEnv } from './utils/env-utils'
 
+const port = portFromEnv('PORT', 13001)
+
 const pgPool = constructPgPool()
 const postgraphileDbSchemas = ['eg_public']
-
-const fastifyInstance = fastify({ logger: true })
 const postgraphileMiddleware = postgraphile(
     pgPool,
     postgraphileDbSchemas,
     generatePostgraphileOptions()
 )
 
+const app = express()
+app.use(postgraphileMiddleware)
+
 if (envVarToBool('EXPORT_GQL_SCHEMA_ONLY')) {
-    fastifyInstance.log.info(
-        'Exporting the schema only. Returning before starting server.'
-    )
+    console.log('Exporting the schema only. Returning before starting server.')
     process.exit(0)
 }
 
-addPostgraphileToFastify(fastifyInstance, postgraphileMiddleware)
-
-const start = async () => {
-    try {
-        await fastifyInstance.listen(portFromEnv('PORT', 13001))
-    } catch (err) {
-        fastifyInstance.log.error(err)
-        process.exit(1)
-    }
-}
-start()
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+})
